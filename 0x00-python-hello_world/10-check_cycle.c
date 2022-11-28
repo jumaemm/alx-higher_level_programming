70600891e8da00d3d946ddacd8247184a3226a61
Emmanuel Juma <jumaemmanuel.EJ@outlook.com>
2022-11-28 10:09:24 +0000

    Check for a cycle in a linked list

---
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -0,0 +1,24 @@
+#include "lists.h"
+
+/**
+ * check_cycle - check if a cycle exists in a linked list
+ * @list: list to be checked
+ * Return: 1 if present, o if none
+ */
+int check_cycle(listint_t *list)
+{
+	listint_t *slow = list;
+	listint_t *fast = list;
+
+	if (list == NULL)
+		return (0);
+
+	while (slow && fast && fast->next)
+	{
+		if (slow == fast)
+			return (1);
+		fast = fast->next->next;
+		slow = slow->next;
+	}
+	return (0);
+}
