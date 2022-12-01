928c194fe6d5e40b84574dc2a742c9cab73f050b
Emmanuel Juma <jumaemmanuel.EJ@outlook.com>
2022-12-01 11:33:29 +0000

    Undeclared vars

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -10,5 +10,5 @@ if __name__ == "__main__":
         print("1 argument:")
     else:
         print("{} arguments:".format(argc))
-    for i in range(count):
-        print("{}: {}".format(argc + 1, sys.argv[i + 1]))
+    for i in range(argc):
+        print("{}: {}".format(i + 1, sys.argv[i + 1]))
