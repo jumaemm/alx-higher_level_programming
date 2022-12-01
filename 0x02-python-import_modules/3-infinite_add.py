c4054d7906e978b37370ec65445dbbeb55922311
Emmanuel Juma <jumaemmanuel.EJ@outlook.com>
2022-12-01 07:18:38 +0000

    Add all args and return sum

---
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+if __name__ == "main":
+    import sys
+
+    argc = len(sys.argv) - 1
+    sum_args = 0
+
+    if argc == 0:
+        print("0")
+    else:
+        for i in range(argc):
+            sum_args += int(sys.argv[i + 1])
+        print("{}".format(sum_args))
