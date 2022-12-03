440d3dc7b2e48a9a61c02fb69e6fbe323a1baeba
Emmanuel Juma <jumaemmanuel.EJ@outlook.com>
2022-12-03 23:23:36 +0000

    and check

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -2,6 +2,6 @@
 def no_c(my_string):
     str_copy = ""
     for i in my_string:
-        if i != 'c' or i != 'C':
+        if i != 'c' and i != 'C':
             str_copy.append(i)
     return str_copy
