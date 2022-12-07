18b726770a3366f4728f0eea0468157c7725822f
Emmanuel Juma <jumaemmanuel.EJ@outlook.com>
2022-12-07 09:18:17 +0000

    Syntax error

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,3 +1,4 @@
 #!/usr/bin/python3
 def square_matrix_ximple(matrix=[]):
-    return [[col ** 2 for col in row] for row in matrix]
+    list2 = [[col ** 2 for col in row] for row in matrix]
+    return list2
