6d246362b7a081e91196340dfa541c7b63b46b5f
Emmanuel Juma <jumaemmanuel.EJ@outlook.com>
2022-12-19 21:53:33 +0000

    Print quotient safely

---
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -0,0 +1,8 @@
+def safe_print_division(a, b):
+    try:
+        quotient = a / b
+    except (TypeError, ZeroDivisionError):
+        quotient = None
+    finally:
+        print("Inside result: {}".format(quotient))
+    return (quotient)
