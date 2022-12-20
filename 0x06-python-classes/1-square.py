51b2367316995c40a08d7e6da6b9de66be2195a1
Emmanuel Juma <jumaemmanuel.EJ@outlook.com>
2022-12-20 12:04:55 +0000

    Private instance attrib

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+
+"""Define a square"""
+
+
+class Square:
+    """Rep a square"""
+
+    def __init__(self, size):
+        """Initialize the square
+
+        Args:
+            size(int): the size of the square
+        """
+
+        self.__size = size
