33302ec813ffddf93cad1c8e56d7787ee70b1a7e
Emmanuel Juma <jumaemmanuel.EJ@outlook.com>
2023-01-09 07:34:39 +0000

    Lookkup dir

---
+++ 0x0A-python-inheritance/0-lookup.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""This module returns properties of on object"""
+
+
+def lookup(obj):
+    """Returns a list of available attributes and methods of an object"""
+
+    return (dir(obj))
