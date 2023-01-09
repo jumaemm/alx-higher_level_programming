7e7afdf11df5fb868d55f855507aa473dd0e40ac
Emmanuel Juma <jumaemmanuel.EJ@outlook.com>
2023-01-09 08:54:10 +0000

    Area implementation

---
+++ 0x0A-python-inheritance/11-square.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""Represents a square subclass of rectangle"""
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """Represents a square"""
+
+    def __init__(self, size):
+        """Initialize the square
+        Args:
+            size (int): side length of the square
+        """
+
+        self.integer_validator("size", size)
+        super().__init__(size, size)
+        self.__size = size
+
