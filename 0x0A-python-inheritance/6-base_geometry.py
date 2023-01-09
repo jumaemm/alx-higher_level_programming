bfa6f1239dacf819b0c3464cbf25c9f1f24880d3
Emmanuel Juma <jumaemmanuel.EJ@outlook.com>
2023-01-09 08:27:32 +0000

    Empty method

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""empty class"""
+
+
+class BaseGeometry():
+    """empty class for now"""
+    
+    def area(self):
+        """Throws exception"""
+
+        raise Exception("area() is not implemented")
