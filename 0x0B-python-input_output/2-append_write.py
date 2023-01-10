4b4768534b818aa00726098190163fbcddf3043d
Emmanuel Juma <jumaemmanuel.EJ@outlook.com>
2023-01-10 10:06:35 +0000

    Append text to end of file

---
+++ 0x0B-python-input_output/2-append_write.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""defines a function to append a string to the end of a file"""
+
+
+def append_write(filename="", text=""):
+    """Append string to the end of a file
+        Arguments:
+            filename (str): name of file
+            text (str): string to be appended
+        Return: number of chars appended
+    """
+    with open(filename, 'a', encoding="utf-8"):
+        return f.write(text)
