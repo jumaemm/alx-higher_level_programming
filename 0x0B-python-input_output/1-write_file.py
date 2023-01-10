309fe8202e5bc62cd4cde98df81037b2dca08acb
Emmanuel Juma <jumaemmanuel.EJ@outlook.com>
2023-01-10 10:01:22 +0000

    Write text to file

---
+++ 0x0B-python-input_output/1-write_file.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""function to srite to a text file"""
+
+
+def write_file(filename="", text=""):
+    """write text data to a file with given filename
+        Arguments:
+            filename (str): name of file to open
+            text (str): text string to write to file
+        Return : number of chars written
+    """
+    with open(filname, "w", encoding="utf-8") as f:
+        return f.write(text)
