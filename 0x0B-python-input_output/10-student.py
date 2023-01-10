f06935fa3b083b66d4704435c1a9d12d6876fe1e
Emmanuel Juma <jumaemmanuel.EJ@outlook.com>
2023-01-10 21:40:30 +0000

    attrs list

---
+++ 0x0B-python-input_output/10-student.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""Contains Student class"""
+
+
+class Student:
+    """Repressents a Student"""
+
+    def __init__(self, first_name, last_name, age):
+        """Initializes a Student object
+            Arguments:
+                first_name (str): first name of the  student
+                last_name (str): last name of the student
+                age (int): age of the student
+        """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """Retrieves a dict representation of a Student instance"""
+        if (type(attrs) == list and
+                all(type(item) == str for item in attrs)):
+            return {i: getattr(self, i) for i in attrs if hasattr(self, i)}
+        return self.__dict__
