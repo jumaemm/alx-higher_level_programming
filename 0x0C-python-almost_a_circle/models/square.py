3add9357d2e4c38e5352e8705c3120e618f28964
Emmanuel Juma <jumaemmanuel.EJ@outlook.com>
2023-01-16 19:03:27 +0000

    bugfix

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -16,6 +16,10 @@ class Square(Rectangle):
         """
         super().__init__(size, size, x, y, id)
 
+    def __str__(self):
+        """Return str rep of the square"""
+        return "[Square] ({}) {}/{} - {}".format(self.id, self.x, self.y,
+                                                 self.width)
     @property
     def size(self):
         """get the size of the Square."""
