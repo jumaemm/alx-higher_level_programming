aef9d42d96b0be30141af69eb6f7ff29c1442cf7
Emmanuel Juma <jumaemmanuel.EJ@outlook.com>
2023-01-16 09:04:01 +0000

    base model

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 """Base model for core functionality"""
+import json
 
 
 class Base:
@@ -9,8 +10,70 @@ class Base:
 
     def __init__(self, id=None):
         """Initialize instance of the Base class"""
-        if (id):
+        if id is not None:
             self.id = id
         else:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
+
+    @staticmethod
+    def to_json_string(list_dictionaries):
+        """Return the JSON string rep of ``list_dictionaries``
+        Arguments:
+            list_dictionaries (dict): a list of dictionaries
+        """
+        if list_dictionaries is None or list_dictionaries == []:
+            return "[]"
+        return json.dumps(list_dictionaries)
+
+    @classmethod
+    def save_to_file(cls, list_objs):
+        """writes JSON string rep to a file
+        Arguments:
+            cls :  class
+            list_objs (list): list of instances who inherit from Base
+        """
+        filename = cls.__name__ + ".json"
+        with open(filename, "w") as f:
+            if list_objs is None:
+                f.write("[]")
+            else:
+                list_dicts = [o.to_dictionary() for o in list_objs]
+                f.write(Base.to_json_string(list_dicts))
+
+    @staticmethod
+    def from_json_string(json_string):
+        """Returns the list of JSON string rep ``json_string``
+        Arguments:
+            json_string (str): string repping a list of dicts
+        """
+        if json_string is None or json_string == "[]":
+            return []
+        return json.loads(json_string)
+
+    @classmethod
+    def create(cls, **dictionary):
+        """returns an instance with all attributes already set
+        Arguments:
+            cls : class
+            **dictionary (dict): k/v pairs of attribs
+        """
+        if dictionary and dictionary != {}:
+            if cls.__name__ == "Rectangle":
+                new_instance = cls(1, 1)
+            else:
+                new_instance = cls(1)
+            new_instance.update(**dictionary)
+            return new_instance
+
+    @classmethod
+    def load_from_file(cls):
+        """Returns a list of instances from a file
+        """
+        filename = str(cls.__name__) + ".json"
+        try:
+            with open(filename, "r") as f:
+                list_dicts = Base.from_json_string(f.read())
+                return [cls.create(**d) for d in list_dicts]
+        except IOError:
+            return []
