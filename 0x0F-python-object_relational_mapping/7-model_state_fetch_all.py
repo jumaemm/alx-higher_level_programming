efb88a8423fcb72df5d9d0a3368a9dd55e4bdc86
Emmanuel Juma <jumaemmanuel.EJ@outlook.com>
2023-03-19 18:08:18 +0000

    Pycodestyle

--- 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -12,7 +12,7 @@ from model_state import Base, State
 
 if __name__ == "__main__":
     engine = create_engine("mysql+mysqldb://{}:{}@localhost/{}"
-                        .format(argv[1], argv[2], argv[3]))
+                           .format(argv[1], argv[2], argv[3]))
     Session = sessionmaker(bind=engine)
     session = Session()
 
