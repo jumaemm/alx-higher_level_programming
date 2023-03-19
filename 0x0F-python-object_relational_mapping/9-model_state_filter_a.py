1148835060ec6f44224918c312c0e95c2c5c1c2d
Emmanuel Juma <jumaemmanuel.EJ@outlook.com>
2023-03-19 19:35:36 +0000

    Refactor to only query

--- 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
+++ 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
@@ -17,6 +17,6 @@ if __name__ == "__main__":
     Session = sessionmaker(bind=engine)
     session = Session()
 
-    for state in session.query(State).order_by(State.id):
-        if "a" in state.name:
-            print("{}: {}".format(state.id, state.name))
+    states = session.query(State).filter(State.name.like('%a%')).all()
+    for state in states:
+        print("{}: {}".format(state.id, state.name))
