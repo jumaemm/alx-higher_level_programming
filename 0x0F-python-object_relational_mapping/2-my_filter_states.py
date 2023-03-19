93a4e0999271eb45e017542254d911b33bd2f8c7
Emmanuel Juma <jumaemmanuel.EJ@outlook.com>
2023-03-19 15:35:05 +0000

    pycodetyle

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -1,13 +1,13 @@
 #!/usr/bin/python3
 """
-Script that takes in an argument and displays entries 
+Script that takes in an argument and displays entries
 in a db where the name matches the arg
 """
 import sys
 import MySQLdb
 
 if __name__ == "__main__":
-    db = db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
+    db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
     cur = db.cursor()
     cur.execute("SELECT * FROM `states` WHERE `name` LIKE '{:s}' ORDER BY \
                 `id` ASC".format(sys.argv[4]))
