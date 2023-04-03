af365fbdb19919111100290d4e6c1e4115d62495
Emmanuel Juma <jumaemmanuel.EJ@outlook.com>
2023-04-03 18:42:17 +0000

    Whitespace

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
-"""Script that takes in a URL and 
+"""Script that takes in a URL and
 displays the X-Request-Id in the response"""
 from sys import argv
 import urllib.request
 
-if __name__=="__main__":
+if __name__ == "__main__":
     url = argv[1]
     req = urllib.request.Request(url)
 
