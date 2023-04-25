c09c4b507489ee957d6a423fa69846148fe08887
Emmanuel Juma <jumaemmanuel.EJ@outlook.com>
2023-04-25 22:11:30 +0000

    fs function

--- 0x14-javascript-web_scraping/5-request_store.js
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -9,6 +9,8 @@ request(URL, function (error, response, body) {
   if (error) {
     console.log(error);
   } else {
-    fs.writeFile(path, body, 'utf-8');
+    fs.writeFile(path, body, 'utf-8', function (err) {
+      if (err) console.log(err);
+    });
   }
 });
